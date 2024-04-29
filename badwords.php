90aa2e23f1163427b4f57f7a7a0c3576752c34d4
samuelescavello <scavellosamuele23@gmail.com>
2024-04-29 16:33:15 +0200

    lunghezza frase

--- badwords.php
+++ badwords.php
@@ -2,6 +2,7 @@
 $frase = $_POST['frase'];
 $badword = $_POST['badword'];
 $sos = str_replace($badword,'***', $frase);
+$lunghezza = strlen ($frase);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +12,7 @@ $sos = str_replace($badword,'***', $frase);
     <title>PHP Badwords</title>
 </head>
 <body>
+    <h2>la lunghezza della frase é di <?php echo $lunghezza ?> caratteri</h2>
     <h2>la tua frase:</h2>
     <p><?php echo $frase ?></p>
     <h2>la tua frase censurata:</h2>
